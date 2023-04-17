494642505691723ba5de8491b6dde0ad7ec4c0b8
Sadegh <m_maghareh@arnika.ai>
2023-04-18 01:07:42 +0330

    All mentioned escape sequence has been added and refactoring

--- HW1/Q1_Hello_World/Q1.c
+++ HW1/Q1_Hello_World/Q1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 //Todo: Q1
 int main() {
-    printf("hello, world\nhello, world");
-    printf("hello, world\thello, world");
-    printf("hello, world\\hello, world");
-    printf("hello, world\"hello, world");
+    printf("1_hello, world\nhello, world\n");
+    printf("2_hello, world\thello, world\n");
+    printf("3_hello, world\\hello, world\n");
+    printf("4_hello, world\"hello, world\n");
 //    printf("hello, world\chello, world");
     return 0;
 }
