4932e7cb198bb60a10691dbc5bcc666e23160b5a
Sadegh <m_maghareh@arnika.ai>
2023-04-18 01:50:33 +0330

    Next Prime number question added

--- HW2/Q1_Next_Price_Number.c
+++ HW2/Q1_Next_Price_Number.c
@@ -1,3 +1,28 @@
-//
-// Created by Mohammad Sadegh Maghareh on 4/18/23.
-//
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1)
+        return 0;
+
+    for (int i = 2; i <= num/2; ++i) {
+        if (num % i == 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+int main() {
+    int n, prime;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    prime = n + 1;
+
+    while (!isPrime(prime)) {
+        prime++;
+    }
+
+    printf("The first prime number after %d is %d", n, prime);
+
+    return 0;
+}
