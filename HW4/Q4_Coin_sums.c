9ba818866210802d814aace830e31e0ff716a241
Sadegh <m_maghareh@arnika.ai>
2023-04-27 01:02:30 +0330

    HW4 Q4 has been added

--- HW4/Q4_Coin_sums.c
+++ HW4/Q4_Coin_sums.c
@@ -1,3 +1,21 @@
-//
-// Created by Mohammad Sadegh Maghareh on 4/27/23.
-//
+#include <stdio.h>
+
+#define TARGET 200  // target amount in pence
+#define COINS 8  // number of coin types
+#define MAX_COINS 1000  // maximum number of coins
+
+int main() {
+    int coin_sizes[COINS] = {1, 2, 5, 10, 20, 50, 100, 200};  // coin sizes in pence
+    int ways[MAX_COINS] = {1};  // initialize table with one way to make zero pence
+    int i, j;
+
+    for (i = 0; i < COINS; i++) {
+        for (j = coin_sizes[i]; j <= TARGET; j++) {
+            ways[j] += ways[j - coin_sizes[i]];
+        }
+    }
+
+    printf("Number of ways to make Â£2: %d", ways[TARGET]);
+
+    return 0;
+}
