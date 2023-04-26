2e51972a08e37ba58e2bf8fe849b14a66d0aa5e0
Sadegh <m_maghareh@arnika.ai>
2023-04-27 00:22:07 +0330

    HW2 has been updated

--- HW2/Q2_Rhumbos.c
+++ HW2/Q2_Rhumbos.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    int n, m, space, star;
-
-    printf("Enter the number of rhombuses to print (1-4): \n");
-    scanf("%d", &m);
-
-    printf("Enter the diameter of each rhombus (odd number between 1-15): \n");
-    scanf("%d", &n);
-    system("clear");
-    if (n % 2 == 0) {
-        printf("Diameter must be an odd number!\n");
-        return 1;
-    }
-    for(int i =0; i < n; i++){
-        for(int t = 0; t < m; t++){
-            star = n -2 * abs(m -i);
-            space = abs(m - i);
-            for(int k = 0; k < space;k++)
-                printf(" ");
-            for(int k = 0; k < star; k++)
-                printf("*");
-            for(int k = 0; k < space; k++)
-                printf(" ");
+int main()
+{
+    int rows,space,star=1,space_prime,num;
+    scanf("%d\n%d",&num,&rows);
+    space_prime = rows;
+    space = rows/2;
+    for(int  i = 0;i<rows;i++){
+        for( int j = 0;j<space;j++){
+            printf(" ");
         }
+        for( int k= 0;k<star;k++){
+            printf("*");
+        }
+        if(num>1){
+            for(int i_prime =1;i_prime<num;i_prime++){
+                for(int j_prime =1;j_prime<=space_prime;j_prime++){
+                    printf(" ");
+                }
+                for(int k_prime =1;k_prime<=star;k_prime++){
+                    printf("*");
+                }
+            }}
         printf("\n");
+
+        if(i < rows/2){
+            space -= 1;
+            space_prime -=2;
+            star += 2;
+        }
+        else{
+            space += 1;
+            space_prime +=2;
+            star -= 2;
+        }
     }
+
+
+
     return 0;
 }
-
-
