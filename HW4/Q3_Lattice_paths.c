75a760feee34b05720a7f8a58bfd07b4659bf670
Sadegh <m_maghareh@arnika.ai>
2023-04-27 01:01:06 +0330

    HW4 Q3 has been added

--- HW4/Q3_Lattice_paths.c
+++ HW4/Q3_Lattice_paths.c
@@ -1,3 +1,21 @@
-//
-// Created by Mohammad Sadegh Maghareh on 4/27/23.
-//
+#include <stdio.h>
+
+long double factorial(int n);
+
+int main()
+{
+    int m, n;
+    scanf("%d\n%d", &m, &n);
+    long double result = factorial(m+n) / (factorial(m) * factorial(n));
+    printf("%.0Lf\n", result);
+    return 0;
+}
+long double factorial(int n)
+{
+    long double result = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
